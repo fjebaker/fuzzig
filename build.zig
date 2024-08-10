9f7b5488dbec9ea46970154eed991cbb3cc7c009
fjebaker <fergusbkr@gmail.com>
2024-08-10 16:25:20 +0100

    feat: added simple benchmark suite

--- build.zig
+++ build.zig
@@ -25,4 +25,15 @@ pub fn build(b: *std.Build) void {
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
+
+    const exe = b.addExecutable(.{
+        .name = "fuzzig",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_cmd = b.addRunArtifact(exe);
+    const benchmark_step = b.step("benchmark", "Run benchmarks.");
+    benchmark_step.dependOn(&run_cmd.step);
 }
