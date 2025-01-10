44c04733c7c0fee3db83672aaaaf4ed03e943156
fjebaker <fergusbkr@gmail.com>
2025-01-10 09:41:35 +0000

    fix: make build compatible with zig 0.14.0-dev.1671+085cc54aa

--- build.zig
+++ build.zig
@@ -14,7 +14,9 @@ pub fn build(b: *std.Build) void {
     );
 
     const lib_unit_tests = b.addTest(.{
-        .root_module = mod,
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
     });
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
